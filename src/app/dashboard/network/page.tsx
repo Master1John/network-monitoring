6045a48b4647c1b2c5ef3aad66920151268ab0b4
Minister <mundestephane13@gmail.com>
2025-06-05 16:19:38 +0100

    ...

--- src/app/dashboard/network/page.tsx
+++ src/app/dashboard/network/page.tsx
@@ -13,44 +13,48 @@ import {
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import useSocketIO from "@/hooks/useSocketIO";
 import { Device, Packet } from "@/types";
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useEffect, useMemo, useState } from "react";
 
 export default function NetworkPage() {
 	const [nodes, setNodes] = useState<Array<Device>>([]);
-	const [data, setData] = useState(null);
+	const [data, setData] = useState<any>(null);
 	const [packets, setPackets] = useState<Array<Packet>>([]);
+	const [remotePackets, setRemotePackets] = useState<Array<Packet>>([]);
 
 	const fetchNetwork = async () => {
 		const response = await fetch("/api/network");
 
-		const data = await response.json();
+		const data = (await response.json()) as { packets: Array<Packet> };
 
 		setData(data);
-		setPackets(data?.pacekets);
+		setPackets([...data.packets, ...packets]);
 	};
+
 	useEffect(() => {
-		fetchNetwork();
 		const socket = useSocketIO();
-
-		socket.join("Admin").listen("NewPackets", (newPackets: Array<Packet>) => {
-			setPackets(packets.concat(newPackets));
-		});
-
-		// handle devices
-		socket
-			.join("Admin")
-			.here(() => {})
-			.listen("Node", (node: Node) => {
-				setNodes((nodes) => [...nodes, { ...node, id: node.socketId }]);
-			})
-			.listen("Nodes", (nodes: Array<Node>) => {
-				setNodes(nodes.map((n) => ({ ...n, id: n.socketId })));
+		fetchNetwork().finally(() => {
+			socket.join("Admin").listen("NewPackets", (newPackets: Array<Packet>) => {
+				const pcts = packets.concat(newPackets);
+				setPackets(pcts);
 			});
+
+			// handle devices
+			socket
+				.join("Admin")
+				.here(() => {})
+				.listen("Node", (node: Node) => {
+					setNodes((nodes) => [...nodes, { ...node, id: node.socketId }]);
+				})
+				.listen("Nodes", (nodes: Array<Node>) => {
+					setNodes(nodes.map((n) => ({ ...n, id: n.socketId })));
+				});
+		});
 
 		return () => {
 			socket.disconnect();
 		};
-	}, [packets]);
+	}, []);
+
 	return (
 		<div className="flex flex-col gap-4">
 			<div className="flex items-center justify-between">
@@ -101,9 +105,7 @@ export default function NetworkPage() {
 							</CardDescription>
 						</CardHeader>
 						<CardContent>
-							<Suspense fallback={<div>Loading network packets...</div>}>
-								<NetwokPackets packets={packets} />
-							</Suspense>
+							<NetwokPackets packets={packets} />
 						</CardContent>
 					</Card>
 
